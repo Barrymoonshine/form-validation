b4a0f414a26d87b693c637f6d59e538153264a5b
Nick Sheward <nickshwrd@gmail.com>
2023-03-24 15:24:46 +0000

    Created check all fields function

--- script.js
+++ script.js
@@ -7,38 +7,59 @@ const formFields = document.getElementsByClassName("form-fields");
 
 const formController = (() => {
   const validateEmail = () => {
-    console.log(emailInput.value);
     const emailRegex =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (emailInput.value.match(emailRegex)) {
       console.log("valid email address ");
-    } else {
-      console.log("invalid email address ");
+      return true;
     }
+    console.log("invalid email address ");
+    return false;
   };
 
   const validatePassword = () => {
-    console.log(passwordInput.value);
     // Please enter a password that is between 8 and 20 characters long and contains at least one number
     // one capital letter and one special symbol(!@#$%^&*=+-_)
     const passwordRegex =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,20}$/;
     if (passwordInput.value.match(passwordRegex)) {
       console.log("valid password ");
-    } else {
-      console.log("invalid password ");
+      return true;
     }
+    console.log("invalid password ");
+    return false;
   };
 
   const checkPasswordsMatch = () => {
     if (confPasswordInput.value === passwordInput.value) {
       console.log("Password match ");
-    } else {
-      console.log("Passwords don't match");
+      return true;
     }
+    console.log("Passwords don't match");
+    return false;
   };
 
-  return { validateEmail, validatePassword, checkPasswordsMatch };
+  const clearFields = () => {
+    passwordInput.value = "";
+    emailInput.value = "";
+    confPasswordInput.value = "";
+    countryInput.value = "America";
+  };
+
+  const checkAllFields = () => {
+    if (validateEmail() && validatePassword() && checkPasswordsMatch()) {
+      console.log("All fields have been completed correctly, form submitted");
+      clearFields();
+    } else {
+      console.log("Some fields have not been completed correctly ");
+    }
+  };
+  return {
+    validateEmail,
+    validatePassword,
+    checkPasswordsMatch,
+    checkAllFields,
+  };
 })();
 
 const displayController = (() => {
@@ -48,8 +69,12 @@ const displayController = (() => {
   passwordInput.addEventListener("blur", () => {
     formController.validatePassword();
   });
-
   confPasswordInput.addEventListener("blur", () => {
     formController.checkPasswordsMatch();
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formController.checkAllFields();
   });
 })();
