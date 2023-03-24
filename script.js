894bf549f43765baea3b3ff5c6e356fb1065e476
Nick Sheward <nickshwrd@gmail.com>
2023-03-24 14:58:09 +0000

    Created validatePassword function and moved all functions into modules

--- script.js
+++ script.js
@@ -5,16 +5,38 @@ const passwordInput = document.getElementById("password");
 const confPasswordInput = document.getElementById("confirm-password");
 const formFields = document.getElementsByClassName("form-fields");
 
-const validateFormFields = () => {
-  const emailRegex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (emailInput.value.match(emailRegex)) {
-    console.log("valid email address ");
-  } else {
-    console.log("invalid email address ");
-  }
-};
+const formController = (() => {
+  const validateEmail = () => {
+    console.log(emailInput.value);
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailInput.value.match(emailRegex)) {
+      console.log("valid email address ");
+    } else {
+      console.log("invalid email address ");
+    }
+  };
 
-emailInput.addEventListener("blur", () => {
-  validateFormFields();
-});
+  const validatePassword = () => {
+    console.log(passwordInput.value);
+    // Please enter a password that is between 8 and 20 characters long and contains at least one number
+    // one capital letter and one special symbol(!@#$%^&*=+-_)
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,20}$/;
+    if (passwordInput.value.match(passwordRegex)) {
+      console.log("valid password ");
+    } else {
+      console.log("invalid password ");
+    }
+  };
+  return { validateEmail, validatePassword };
+})();
+
+const displayController = (() => {
+  emailInput.addEventListener("blur", () => {
+    formController.validateEmail();
+  });
+  passwordInput.addEventListener("blur", () => {
+    formController.validatePassword();
+  });
+})();
