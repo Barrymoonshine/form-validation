28562f625014414a77fc484455dbfb64626f70f9
Nick Sheward <nickshwrd@gmail.com>
2023-03-28 15:54:01 +0100

    Basic styling applied to page

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const formController = (() => {
   const addClassName = (e) => {
-    e.target.className = "field-selected";
+    e.target.className = 'field-selected';
   };
 
   const validateEmail = () => {
-    const isEmailValid = document.getElementById("email").validity.valid;
+    const isEmailValid = document.getElementById('email').validity.valid;
     if (isEmailValid) {
       return true;
     }
@@ -14,22 +14,22 @@ const formController = (() => {
   };
 
   const addZipCodeRegex = () => {
-    const country = document.getElementById("country").value;
-    const zipCode = document.getElementById("zip-code");
-    if (country === "US") {
-      zipCode.pattern = "^d{5}(?:[-s]d{4})?$";
+    const country = document.getElementById('country').value;
+    const zipCode = document.getElementById('zip-code');
+    if (country === 'US') {
+      zipCode.pattern = '^d{5}(?:[-s]d{4})?$';
     }
-    if (country === "UK") {
-      zipCode.pattern = "^[A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][A-Z]{2}$";
+    if (country === 'UK') {
+      zipCode.pattern = '^[A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][A-Z]{2}$';
     }
-    if (country === "FR") {
+    if (country === 'FR') {
       zipCode.pattern =
-        "^(?:(?:(?:0[1-9]|[1-8]d|9[0-4])(?:d{3})?)|97[1-8]|98[4-9]|‌​‌​2[abAB])$";
+        '^(?:(?:(?:0[1-9]|[1-8]d|9[0-4])(?:d{3})?)|97[1-8]|98[4-9]|‌​‌​2[abAB])$';
     }
   };
 
   const validateZipCode = () => {
-    const isZipCodeValid = document.getElementById("zip-code").validity.valid;
+    const isZipCodeValid = document.getElementById('zip-code').validity.valid;
     if (isZipCodeValid) {
       return true;
     }
@@ -39,7 +39,7 @@ const formController = (() => {
   };
 
   const validatePassword = () => {
-    const isPasswordValid = document.getElementById("password").validity.valid;
+    const isPasswordValid = document.getElementById('password').validity.valid;
     if (isPasswordValid) {
       return true;
     }
@@ -49,15 +49,15 @@ const formController = (() => {
   };
 
   const checkPasswordsMatch = () => {
-    const passwordValue = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirm-password");
+    const passwordValue = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password');
     const confirmPasswordValue = confirmPassword.value;
     if (passwordValue === confirmPasswordValue) {
-      confirmPassword.setCustomValidity("");
+      confirmPassword.setCustomValidity('');
       return true;
     }
     if (passwordValue !== confirmPasswordValue) {
-      confirmPassword.setCustomValidity("Passwords do not match");
+      confirmPassword.setCustomValidity('Passwords do not match');
       return false;
     }
   };
@@ -65,18 +65,18 @@ const formController = (() => {
   const clearFields = () => {
     const field = Array.from(form.elements);
     field.forEach((item) => {
-      item.value = "";
-      item.className = "field submitted";
+      item.value = '';
+      item.className = 'field submitted';
     });
   };
 
   const checkAllFields = (e) => {
     if (validateEmail() && validatePassword() && checkPasswordsMatch()) {
-      console.log("All fields have been completed correctly, form submitted");
+      alert('Awesome you did, all fields are completed correctly!');
       clearFields();
     } else {
       e.stopImmediatePropagation();
-      console.log("Some fields have not been completed correctly!! ");
+      alert('Some fields are not completed correctly, have another go :)');
     }
   };
   return {
@@ -91,19 +91,19 @@ const formController = (() => {
 })();
 
 const displayController = (() => {
-  const form = document.getElementById("form");
+  const form = document.getElementById('form');
 
-  form.addEventListener("keyup", (e) => {
-    if (e.target.id === "email") {
+  form.addEventListener('keyup', (e) => {
+    if (e.target.id === 'email') {
       formController.validateEmail();
       formController.addClassName(e);
-    } else if (e.target.id === "zip-code") {
+    } else if (e.target.id === 'zip-code') {
       formController.validateZipCode();
       formController.addClassName(e);
-    } else if (e.target.id === "password") {
+    } else if (e.target.id === 'password') {
       formController.validatePassword();
       formController.addClassName(e);
-    } else if (e.target.id === "confirm-password") {
+    } else if (e.target.id === 'confirm-password') {
       formController.checkPasswordsMatch();
       formController.addClassName(e);
     } else {
@@ -111,13 +111,13 @@ const displayController = (() => {
     }
   });
 
-  form.addEventListener("click", (e) => {
-    if (e.target.id === "country") {
+  form.addEventListener('click', (e) => {
+    if (e.target.id === 'country') {
       formController.addZipCodeRegex();
     }
   });
 
-  form.addEventListener("submit", (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
     formController.checkAllFields(e);
   });
